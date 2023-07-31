104795828c1ae11e460a12db0d4286fb92c777ae
subak96 <139088858+subak96@users.noreply.github.com>
2023-07-31 20:05:04 +0900

    액티비티 이동

--- activity/app/src/main/java/com/example/activity/FirstActivity.kt
+++ activity/app/src/main/java/com/example/activity/FirstActivity.kt
@@ -1,11 +1,21 @@
 package com.example.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        val btn = findViewById<Button>(R.id.btn_1)
+
+        btn.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- activity/app/src/main/java/com/example/activity/SecondActivity.kt
+++ activity/app/src/main/java/com/example/activity/SecondActivity.kt
@@ -2,10 +2,19 @@ package com.example.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val btn = findViewById<Button>(R.id.bnt_close)
+
+        btn.setOnClickListener{
+            finish()
+        }
+
+
     }
 }
