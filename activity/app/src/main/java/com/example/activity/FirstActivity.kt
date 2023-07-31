693bf78b48cb93b1fc27e3a6299418ddc70766a5
subak96 <139088858+subak96@users.noreply.github.com>
2023-07-31 20:15:57 +0900

    버튼 추가 및 동작 추가

--- activity/app/src/main/java/com/example/activity/FirstActivity.kt
+++ activity/app/src/main/java/com/example/activity/FirstActivity.kt
@@ -1,8 +1,10 @@
 package com.example.activity
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
@@ -16,6 +18,18 @@ class FirstActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+    }
+    fun doOnBtnClick(view: View){
+        when(view.getId()) {
+            R.id.buttonDialActivity -> {
+                val  call_intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:114"))
+                startActivity(call_intent)
+            }
 
+            R.id.buttonMapActivity -> {
+                val  map_intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.565350, 127.01445"))
+                startActivity(map_intent)
+            }
+        }
     }
 }
