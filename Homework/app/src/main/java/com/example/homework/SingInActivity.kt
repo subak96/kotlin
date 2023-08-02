880925fe2e4dee50e234ac64e0fac658134643f8
subak96 <139088858+subak96@users.noreply.github.com>
2023-08-02 20:34:04 +0900

    아아디, 비밀번호 입력 안할시 Toast 출력 기능 완성

--- Homework/app/src/main/java/com/example/homework/SingInActivity.kt
+++ Homework/app/src/main/java/com/example/homework/SingInActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class SingInActivity : AppCompatActivity() {
@@ -11,17 +12,30 @@ class SingInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_singinactivity)
 
-        val btn1 = findViewById<Button>(R.id.btn_2)
+        val btn1 = findViewById<Button>(R.id.btn_1)
         btn1.setOnClickListener{
+            val editTextID = findViewById<EditText>(R.id.Password_3)
+            val editTextPassword = findViewById<EditText>(R.id.Password_4)
+
+            val useID = editTextID.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (useID.isEmpty() && password.isEmpty()){
+                Toast.makeText(this, "아이디와 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        val btn2 = findViewById<Button>(R.id.btn_2)
+        btn2.setOnClickListener{
             val intent = Intent(this, SignpActivity::class.java)
             startActivity(intent)
         }
 
 
-        val btn2 = findViewById<Button>(R.id.btn_1)
-        btn2.setOnClickListener{
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
     }
+
 }
