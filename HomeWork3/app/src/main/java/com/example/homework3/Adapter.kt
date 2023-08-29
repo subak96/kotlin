de054d8a6fd62b95d9170d245f80c256bca95f3f
subak96 <139088858+subak96@users.noreply.github.com>
2023-08-30 05:27:39 +0900

    개인과제 - 노티펑션,백버튼활성화,인텐트,상세페이지 이동 만들기

--- HomeWork3/app/src/main/java/com/example/homework3/Adapter.kt
+++ HomeWork3/app/src/main/java/com/example/homework3/Adapter.kt
@@ -1,20 +1,37 @@
 package com.example.homework3
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.homework3.databinding.ListBinding
+import java.text.DecimalFormat
 
-class Adapter (val Item:MutableList<Data>) : RecyclerView.Adapter<Adapter.Holder>() {
+class Adapter (val Item:MutableList<Date>) : RecyclerView.Adapter<Adapter.Holder>() {
 
+    interface ItemClick {
+        fun onClick(view : View, position : Int)
+    }
+
+    var itemClick : ItemClick? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ListBinding.inflate(LayoutInflater.from(parent.context),parent, false)
         return Holder(binding)
     }
     override fun onBindViewHolder(holder: Holder, position: Int) {
-       holder.ImageView.setImageResource(Item[position].Icon)
-        holder.name.text = Item[position].Name
-        holder.Position.text = Item[position].Position
+       holder.ImageView.setImageResource(Item[position].ImageView)
+        holder.Tite.text = Item[position].itemTite
+        holder.Position.text = Item[position].c
+
+        val price = Item[position].price
+        holder.price.text = DecimalFormat("#,###").format(price)+"원"
+        holder.heart.text = Item[position].Heart.toString()
+        holder.chat.text = Item[position].Chat.toString()
+
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
     }
     override fun getItemCount(): Int {
         return Item.size
@@ -22,7 +39,10 @@ class Adapter (val Item:MutableList<Data>) : RecyclerView.Adapter<Adapter.Holder
 
     inner class Holder( val binding: ListBinding) : RecyclerView.ViewHolder(binding.root){
         val ImageView = binding.iconItem
-        val name = binding.textItem1
+        val Tite = binding.textItem1
         val Position = binding.textItem2
+        val price = binding.textItem3
+        val heart = binding.heart
+        val chat = binding.chat
     }
 }
---
+++ HomeWork3/app/src/main/java/com/example/homework3/Date.kt
@@ -0,0 +1,13 @@
+package com.example.homework3
+
+data class Date (
+    val ImageView:Int,
+    val itemTite:String,
+    val itemPosition:String,
+    val a:String,
+    val price:Int,
+    val c:String,
+    val Chat:Int,
+    val Heart:Int
+) {
+}
